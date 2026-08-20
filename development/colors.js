6d38aeaf16b7a804dac78a6649e2716a3d6a92b4
itswiktoragain <propalnie15@gmail.com>
2026-08-20 23:39:13 +0200

    Simplify gh-pages to static gallery only

--- development/colors.js
+++
@@ -1,7 +0,0 @@
-const enableColor = !process.env.NO_COLOR;
-const color = (i) => (enableColor ? i : "");
-
-export const RESET = color("\x1b[0m");
-export const BOLD = color("\x1b[1m");
-export const RED = color("\x1b[31m");
-export const GREEN = color("\x1b[32m");
